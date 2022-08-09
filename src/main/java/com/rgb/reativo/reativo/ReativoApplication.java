f27a6d6c5f4c045945c89ac033336e837894f70e
Renam Bulhoes <renamgustavo@live.com>
2022-08-09 20:48:22 -0300

    Iniciando projeto

---
+++ src/main/java/com/rgb/reativo/reativo/ReativoApplication.java
@@ -0,0 +1,13 @@
+package com.rgb.reativo.reativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativoApplication.class, args);
+	}
+
+}
