2df8ae70eab950da286bc298dc3329853ad0b789
Renam Bulhoes <renamgustavo@live.com>
2022-08-09 21:57:23 -0300

    pubsub

--- src/main/java/com/rgb/reativo/reativo/repositories/Database.java
+++ src/main/java/com/rgb/reativo/reativo/repositories/Database.java
@@ -1,2 +1,10 @@
-package com.rgb.reativo.reativo.repositories;public interface Database {
+package com.rgb.reativo.reativo.repositories;
+
+import java.util.Optional;
+
+public interface Database {
+
+    <T> T save(final String key, final T value);
+
+    <T> Optional<T> get(final String key, final Class<T> clazz);
 }
--- src/main/java/com/rgb/reativo/reativo/repositories/PaymentRepository.java
+++ src/main/java/com/rgb/reativo/reativo/repositories/PaymentRepository.java
@@ -1,2 +1,33 @@
-package com.rgb.reativo.reativo.repositories;public class PaymentRepository {
+package com.rgb.reativo.reativo.repositories;
+
+import com.rgb.reativo.reativo.models.Payment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentRepository {
+    private final Database database;
+
+    public Mono<Payment> createPayment(final String userId) {
+        final Payment payment = Payment.builder()
+                .id(UUID.randomUUID().toString())
+                .userId(userId)
+                .status(Payment.PaymentStatus.PENDING)
+                .build();
+
+        return Mono.fromCallable(() -> {
+                    log.info("Saving payment transaction for user {}", userId);
+                    return this.database.save(userId, payment);
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(next -> log.info("Payment received {}", next.getUserId()));
+    }
+
 }
